5d59fecf424ccf47805d36d4d144790023c3ccb3
meew0 <blactbt@live.de>
2014-04-21 19:27:27 +0200

    Added symmetry calculator item

--- src/main/java/meew0/sc/SymcalcMod.java
+++ src/main/java/meew0/sc/SymcalcMod.java
@@ -1,9 +1,13 @@
 package meew0.sc;
 
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import meew0.sc.items.ItemSymmetryCalculator;
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.item.Item;
 import thaumcraft.common.config.ConfigBlocks;
 
 @Mod(modid = SymcalcMod.MODID, version = SymcalcMod.VERSION, name = SymcalcMod.NAME)
@@ -13,11 +17,12 @@ public class SymcalcMod
     public static final String VERSION = "0.01";
     public static final String NAME = "SymCalc";
 
+    public static Item itemSymCalc;
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void preInit(FMLPreInitializationEvent event)
     {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-        System.out.println("TABLE >> "+ ConfigBlocks.blockTable.getUnlocalizedName());
+        itemSymCalc = new ItemSymmetryCalculator().setUnlocalizedName("symmetryCalculator").setTextureName("symcalc:symmetry_calculator").setMaxStackSize(1);
+        GameRegistry.registerItem(itemSymCalc, "symmetryCalculator");
     }
 }
---
+++ src/main/java/meew0/sc/items/ItemSymmetryCalculator.java
@@ -0,0 +1,10 @@
+package meew0.sc.items;
+
+import net.minecraft.item.Item;
+
+/**
+ * Created by meew0 on 21.04.14.
+ */
+public class ItemSymmetryCalculator extends Item {
+
+}
